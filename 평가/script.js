c2a5f7e65b28cb206e95e60a883fe0534c575d05
Nameunseo <nes040612@gmail.com>
2026-05-01 16:29:09 +0900

    Delete 평가/script.js

--- 평가/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-        $(".popup").hide();
-        $(".icon").click(function () {
-            $(".popup").fadeIn();
-        });
-        $(".close-btn").click(function () {
-            $(".popup").fadeOut();
-        });
-    })
